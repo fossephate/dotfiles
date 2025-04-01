e3bc0a3ef6287554ae7c6f6c729633333b7997bf
Matthew Fosse <matt@fosse.co>
2025-03-31 18:27:28 -0700

    add configs

---
+++ sketchybar/plugins/aerospace_windows.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source $CONFIG_DIR/utils/aerospace.sh
+
+if [[ "$SENDER" = "change-window-workspace" ]]; then
+
+    # ! -z means "not empty" - ie the FOCUSED_WORKSPACE was specified
+    if [[  "$FOCUSED_WORKSPACE" ]]; then
+      sketchybar --set workspace.$FOCUSED_WORKSPACE drawing=on \
+        label="$(workspace_app_icons $FOCUSED_WORKSPACE)" 
+    fi
+    if [[ "$TARGET_WORKSPACE" ]]; then
+      sketchybar --set workspace.$TARGET_WORKSPACE drawing=on \
+        label="$(workspace_app_icons $TARGET_WORKSPACE)"
+    fi
+elif [ "$SENDER" = "aerospace_workspace_change" ]; then
+  handle_workspace_change
+fi
