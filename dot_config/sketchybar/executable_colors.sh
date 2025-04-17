82242e02e48972bb7a1b416bc390629fc7c45906
Matthew Fosse <matt@fosse.co>
2025-04-17 13:55:10 -0700

    Initial commit

---
+++ dot_config/sketchybar/executable_colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ~/.config/themes/rose-pine.sh
+
+export BAR_COLOR="0xFF$BASE"     # base
+export ITEM_BG_COLOR="0xff$SURFACE" # surface
+export ACCENT_COLOR="0xff$GOLD"  # iris
+export TEXT_COLOR="0xff$TEXT"    # text
+
---
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Load the color variables
+source $CONFIG_DIR/colors.sh
+source $CONFIG_DIR/utils/aerospace.sh
+
+
