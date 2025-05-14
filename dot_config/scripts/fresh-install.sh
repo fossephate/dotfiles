b26115fcae1e35c37598d8c5c79b2769f648695d
fossephate <matt@fosse.co>
2025-05-14 12:29:45 -0700

    updates-env

--- dot_config/scripts/fresh-install.sh
+++ dot_config/scripts/fresh-install.sh
@@ -1,23 +1,30 @@
 brew tap FelixKratz/formulae
 brew install borders
-brew install --cask cursor
-brew install --cask raycast
+
 brew install sketchybar
-brew install --cask 1password
-brew install --cask warp
-brew install --cask karabiner-elements
-brew install --cask orion
-brew install --cask nikitabobko/tap/aerospace
+
 brew tap leoafarias/fvm
 brew install fvm
-brew install --cask menuwhere
-brew install nvm
 
+brew install nvm
+brew install pnpm
+brew install deno
+
+brew install --cask orion
 brew install --cask xcodes
 brew install --cask slack
 brew install --cask discord
 brew install --cask spotify
 brew install --cask arc
+brew install --cask 1password
+brew install --cask warp
+brew install --cask karabiner-elements
+brew install --cask cursor
+brew install --cask raycast
+brew install --cask menuwhere
+brew install --cask homerow
+
+brew install --cask nikitabobko/tap/aerospace
 
 
 # scutil --get HostName 
